a3ca094c5c9ee38a4efcdcd0de0ec96bf518cc07
Jonathan Parker <jprkr21@gmail.com>
2022-03-17 17:30:25 -0500

    Adding authentication to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,6 @@
 const { Book, User } = require("../models");
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 // User:
 // Query: getSingleUser
@@ -17,7 +19,25 @@ const resolvers = {
   Mutation: {
     createUser: async (parent, args) => {
       const user = await User.create(args);
-      return user;
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
     },
     saveBook: async (parent, { user, body }) => {
       const updatedUser = await Book.findOneAndUpdate(
